27a99823a99b0556f80a5ced8c2838d4050d754b
clearlynow <59779189+clearlynow@users.noreply.github.com>
2021-04-15 10:49:25 -0600

    Initial Commit

---
+++ Plastic/PlasticApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlasticApp.swift
+//  Plastic
+//
+//  Created by Alison Gorman on 4/15/21.
+//
+
+import SwiftUI
+
+@main
+struct PlasticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
